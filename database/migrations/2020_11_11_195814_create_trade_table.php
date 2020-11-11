f05adb639ccdab8726d569f543b97fa031a982a7
ThowV <thomasvisscher@outlook.com>
2020-11-11 23:43:07 +0100

    add trade migration

---
+++ database/migrations/2020_11_11_195814_create_trade_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTradeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trade', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('creator_id');
+            $table->foreignId('responder_id')->nullable();
+            $table->enum('trade_type', ['offer', 'request']);
+            $table->enum('hydrogen_type', ['green', 'blue', 'grey']);
+            $table->boolean('open')->default(true);
+            $table->unsignedBigInteger('units_per_hour')->default(0);
+            $table->unsignedBigInteger('volumes')->default(0);
+            $table->unsignedBigInteger('price_per_unit')->default(0);
+            $table->tinyInteger('mix_co2')->default(0);
+            $table->dateTime('ends_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trade');
+    }
+}
