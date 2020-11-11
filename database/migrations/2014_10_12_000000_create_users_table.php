15198f85f9b355e7c95206e0bf66e014788cd13f
ThowV <thomasvisscher@outlook.com>
2020-11-11 20:42:41 +0100

    add user migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->foreignId('company_id');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('picture_url')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
