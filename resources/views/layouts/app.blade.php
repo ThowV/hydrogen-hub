ecf5bf8c4f8595979da547e3d35661fb85ea6c53
martijnjongman <martijnjongman9@gmail.com>
2020-11-12 12:56:21 +0100

    style the navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,34 +7,35 @@
     <title>Hydrogen Hub</title>
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <link href="http://fonts.cdnfonts.com/css/effra" rel="stylesheet">
+        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 
     <!-- Styles -->
     <style>
         body {
-            font-family: 'Nunito';
-            padding: 4em;
+            font-family: 'Effra', sans-serif;
         }
-        
-        .navbar{
-                color: white;
-                background: blue;
-                width: 20%;
-            }
+        #menu{
+            background: #003399;
+        }
     </style>
     @stack('styles')
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     @livewireStyles
 </head>
-    <body>
-        <div class="w1/2">
-            @include('layouts.nav')
+    <body class="h-screen">
+
+        <div class="grid grid-cols-8 h-screen">
+
+            <div class="col-span-1">
+                    @include('layouts.nav')
+            </div>
+
+            @yield('content')
+            @livewireScripts
+            @stack('scripts')
+        
         </div>
 
-    @yield('content')
-    @livewireScripts
-
-    @stack('scripts')
     </body>
 </html>
