a0661bcd1405e7aab24e2acf9b38f862faa97018
Melchior <melchiorkokernoot@gmai.com>
2020-11-12 16:16:18 +0100

    Basic permissions and role bindings in place

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,10 @@ namespace Database\Seeders;
 use App\Models\Company;
 use App\Models\CompanyDayLog;
 use App\Models\Trade;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,5 +24,36 @@ class DatabaseSeeder extends Seeder
         CompanyDayLog::factory(50)->create();
         Trade::factory(50)->create();
 
+        $this->createPermissions();
+        $this->createRoles();
+        $this->bindRolesToPermissions();
+    }
+
+    public function createPermissions()
+    {
+        Permission::insert([
+            ["name" => "request.allow", "guard_name" => "web", "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
+            ["name" => "request.deny", "guard_name" => "web", "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
+        ]);
+    }
+
+    public function bindRolesToPermissions()
+    {
+        $super_admin = Role::findByName("Super Admin");
+        $super_admin->givePermissionTo('request.allow');
+        $super_admin->givePermissionTo('request.deny');
+
+    }
+
+    public function createRoles()
+    {
+        Role::insert([
+            ["name" => "Super Admin", "guard_name" => "web", "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
+            ["name" => "Admin", "guard_name" => "web", "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
+            ["name" => "Portfolio Manager", "guard_name" => "web", "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
+            ["name" => "Trader", "guard_name" => "web", "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
+            ["name" => "Analyst", "guard_name" => "web", "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
+            ["name" => "Spectator", "guard_name" => "web", "created_at" => Carbon::now(), "updated_at" => Carbon::now()],
+        ]);
     }
 }
