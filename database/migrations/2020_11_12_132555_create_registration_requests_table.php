b8145b3d730ad031250be656316b6b5eb31417b3
Melchior <melchiorkokernoot@gmai.com>
2020-11-12 15:02:38 +0100

    add company registration

---
+++ database/migrations/2020_11_12_132555_create_registration_requests_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrationRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registration_requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('company_email');
+            $table->string('company_admin_first_name');
+            $table->string('company_admin_last_name');
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registration_requests');
+    }
+}
