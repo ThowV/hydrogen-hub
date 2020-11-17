b3c319725a7da36840ebf5f3ffcbbe6b3455e57d
Melchior <melchiorkokernoot@gmai.com>
2020-11-17 10:28:26 +0100

    create an event for permissiondenied so there is a single handler.

--- app/Http/Controllers/RegistrationRequestController.php
+++ app/Http/Controllers/RegistrationRequestController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\CreateCompanyAction;
+
+use App\Events\PermissionDenied;
 use App\Models\RegistrationRequest;
 
 class RegistrationRequestController extends Controller
@@ -10,8 +12,9 @@ class RegistrationRequestController extends Controller
     public function accept(RegistrationRequest $registrationRequest, CreateCompanyAction $action)
     {
         if (!auth()->user()->hasPermissionTo('request.allow')) {
-
-            return;
+            //Notify user that he has no rights to perform this action.
+            \event(new PermissionDenied());
+            return back();
         }
         $action->execute($registrationRequest);
 
