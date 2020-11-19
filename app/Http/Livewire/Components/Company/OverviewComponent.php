1e5f5b6c10c6be0deddff7e982690ac05dde669e
Melchior <melchiorkokernoot@gmai.com>
2020-11-19 12:53:57 +0100

    add functionality to admin -> overview

--- app/Http/Livewire/Components/Company/OverviewComponent.php
+++ app/Http/Livewire/Components/Company/OverviewComponent.php
@@ -2,16 +2,48 @@
 
 namespace App\Http\Livewire\Components\Company;
 
+use App\Models\User;
 use Livewire\Component;
 
 class OverviewComponent extends Component
 {
 
     public $employees;
+    public $updateMode;
+    public $employeeToUpdate;
+
+    protected $rules = [
+        'employeeToUpdate.first_name' => 'required',
+        'employeeToUpdate.last_name' => 'required',
+        'employeeToUpdate.email' => 'required',
+        'employeeToUpdate.picture_url' => 'required',
+        'employeeToUpdate.created_at' => 'required'
+    ];
+
+    protected $listeners = ["updateModeEnabled" => "updateMode"];
+
+    public function updateMode(User $user)
+    {
+        $this->updateMode = true;
+        $this->employeeToUpdate = $user;
+    }
+
+    public function saveUpdate()
+    {
+        $this->validate();
+        $this->employeeToUpdate->save();
+        $this->mount();
+    }
+
+    public function getEmployees()
+    {
+        $this->employees = auth()->user()->company->employees;
+    }
 
     public function mount()
     {
-        $this->employees = auth()->user()->company->employees;
+        $this->updateMode = false;
+        $this->getEmployees();
     }
 
     public function render()
--- resources/views/livewire/components/company/overview-component.blade.php
+++ resources/views/livewire/components/company/overview-component.blade.php
@@ -1,5 +1,6 @@
 <div>
     <h1 class="text-xl">Employee list component</h1>
+
     <table class="table w-full border">
         <tr>
             <th>Employee avatar</th>
@@ -10,6 +11,26 @@
             <th>Delete</th>
         </tr>
         <tbody>
+        @if($updateMode)
+            <tr class="text-center">
+                <form wire:submit.stop>
+                    <td align="center">
+                        <input wire:model="employeeToUpdate.picture_url" type="text"/>
+                    </td>
+                    <td>
+                        <input wire:model="employeeToUpdate.first_name" type="text"/>
+                        <input wire:model="employeeToUpdate.last_name" type="text"/>
+                    </td>
+                    <td>
+                        <input wire:model="employeeToUpdate.email" type="text"/></td>
+                    <td></td>
+                    <td></td>
+                    <td>
+                        <button wire:click="saveUpdate" type="submit" class="p-2 bg-green-500 rounded text-white" autofocus>Submit</button>
+                    </td>
+                </form>
+            </tr>
+        @endif
         @foreach($employees as $employee)
             <tr class="text-center">
                 <td align="center">
@@ -19,16 +40,19 @@
                 <td>{{$employee->email}}</td>
                 <td>{{$employee->created_at}}</td>
                 <td>
-                    <a href="{{ route('employees.update', [$employee->id]) }}" class="rounded p-2 bg-yellow-500">
+                    <a wire:click="$emitUp('updateModeEnabled', {{$employee->id}})" class="rounded p-2 bg-yellow-500">
                         <button>Update</button>
                     </a>
                 </td>
                 <td>
-                    <form action="{{ route('employees.destroy',$employee->id) }}" method="post" class="rounded p-2 bg-red-500">
-                        <button>Delete</button>
-                        @method('delete')
-                        @csrf
-                    </form>
+                    @if(!$employee->id == auth()->id() && !$employee->hasRole(['Super Admin','Admin']))
+                        <form action="{{ route('employees.destroy',$employee->id) }}"
+                              method="post" class="rounded p-2 bg-red-500">
+                            <button>Delete</button>
+                            @method('delete')
+                            @csrf
+                        </form>
+                    @endif
                 </td>
             </tr>
         @endforeach
