fcf84e45c225f40c851f5c95b3b87c0f3ff78afb
Melchior <melchiorkokernoot@gmai.com>
2020-11-19 09:06:12 +0100

    add view stubs

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -8,16 +8,16 @@ class CompanyController extends Controller
 {
     public function index()
     {
-
+        return view('company.index');
     }
 
     public function portfolio()
     {
-
+        return view('company.portfolio');
     }
 
     public function overview()
     {
-
+        return view('company.overview');
     }
 }
---
+++ resources/views/company/overview.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="text-2xl">Company overview</h1>
+@endsection()
