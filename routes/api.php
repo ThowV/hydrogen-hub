ae7c2f6898571fd0c330ae9a0fe16e36c7c79d44
Melchior <melchiorkokernoot@gmai.com>
2020-11-26 14:23:16 +0100

    revert unused changes

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/deploy', [\App\Http\Controllers\DeploymentController::class, 'deploy'])->name('deploy');
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
