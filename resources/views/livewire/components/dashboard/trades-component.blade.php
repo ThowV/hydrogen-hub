6fd15aec7f008ed73761e91df0dd3aa57fb68ea2
ThowV <thomasvisscher@outlook.com>
2020-12-01 12:15:38 +0100

    improve file structure

--- resources/views/livewire/components/dashboard/trades-component.blade.php
+++ resources/views/livewire/components/dashboard/trades-component.blade.php
@@ -1,3 +1,5 @@
 <div>
-    <p>Trades</p>
+    <div class="w-full h-24 xxl:h-32 grid grid-rows-1 grid-cols-2">
+        <h2 class="grid items-center text-xl xxl:text-3xl font-bold">Trades</h2>
+    </div>
 </div>
