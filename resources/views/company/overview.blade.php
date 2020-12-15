140cc5ab3155b2378d937d0dd2ad230f7ac99792
Martijn Jongman <martijnjongman9@gmail.com>
2020-12-15 15:35:13 +0100

    add active navigation

--- resources/views/company/overview.blade.php
+++ resources/views/company/overview.blade.php
@@ -5,7 +5,7 @@
         <!--Header-->
         <div class="w-full h-24 grid grid-col-2 grid-rows-2">
             <div class="col-start-1 flex flex-row items-baseline py-8 xxl:py-10">
-                <h1 class="font-bold text-2xl xxl:text-4xl mr-4 pl-10 xxl:pl-20">Company</h1>
+                <h1 class="font-bold text-2xl xxl:text-4xl mr-4 pl-10 xxl:pl-20">Company overview</h1>
                 <h2 class="font-bold text-xs xxl:text-xl text-gray-600">Short Term Trading</h2>
             </div>
             <div class="col-start-2 grid justify-end py-4 px-10 xxl:py-8">
