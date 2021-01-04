f5529ec7b100fdd8cef87905f9e7e4d1c2a592ea
Melchior <melchiorkokernoot@gmai.com>
2021-01-04 14:37:15 +0100

    add click on logo to toggle form

--- resources/views/livewire/components/company/change-company-logo-component.blade.php
+++ resources/views/livewire/components/company/change-company-logo-component.blade.php
@@ -1,5 +1,4 @@
 <div class="bg-white">
-    <img class="h-16" src="{{ auth()->user()->company->logo_path }}" alt="">
     @if($formOpen)
         <form wire:submit.prevent="save" class="flex flex-col">
             <input type="file" wire:model="photo">
@@ -9,7 +8,12 @@
             <button class="m-auto bg-hovBlue text-white px-6 py-1 rounded sm:text-xxs md:text-xs lg:text-xs xl:text-xs xxl:text:lg hover:bg-nav transaction duration-300" type="submit">Save Photo</button>
         </form>
     @else
-        <button class="underline cursor-pointer" wire:click="toggleForm">Upload picture here</button>
+        @if(isset(auth()->user()->company->logo_path))
+
+            <img wire:click="toggleForm" class="h-16" src="{{ auth()->user()->company->logo_path }}" alt="">
+        @else
+            <button class="underline cursor-pointer" wire:click="toggleForm">Upload picture here</button>
+        @endif
     @endif
 
     <div wire:loading wire:target="photo">Uploading...</div>
