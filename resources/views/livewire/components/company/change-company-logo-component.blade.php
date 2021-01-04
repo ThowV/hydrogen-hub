91233e30c0fe50542c54c30775b2e9d3f25d62eb
Martijn Jongman <martijnjongman9@gmail.com>
2021-01-04 13:55:48 +0100

    add fix style logo

--- resources/views/livewire/components/company/change-company-logo-component.blade.php
+++ resources/views/livewire/components/company/change-company-logo-component.blade.php
@@ -1,16 +1,18 @@
-<div class="bg-white">
-    <img class="h-16" src="{{ auth()->user()->company->logo_path }}" alt="">
-    @if($formOpen)
-        <form wire:submit.prevent="save" class="flex flex-col">
-            <input type="file" wire:model="photo">
-
-            @error('photo') <span class="error text-red-600">{{ $message }}</span> @enderror
-
-            <button class="m-auto bg-hovBlue text-white px-6 py-1 rounded sm:text-xxs md:text-xs lg:text-xs xl:text-xs xxl:text:lg hover:bg-nav transaction duration-300" type="submit">Save Photo</button>
-        </form>
-    @else
-        <button class="underline cursor-pointer" wire:click="toggleForm">Upload picture here</button>
-    @endif
+<div class="bg-white flex">
+    <img class="h-16 w-16 rounded-full cursor-pointer" src="{{ auth()->user()->company->logo_path }}" alt="">
 
     <div wire:loading wire:target="photo">Uploading...</div>
+
+        @if($formOpen)
+            <form wire:submit.prevent="save" class="flex flex-col">
+                <input type="file" wire:model="photo">
+
+                @error('photo') <span class="error text-red-600">{{ $message }}</span> @enderror
+
+                <button class="m-auto bg-hovBlue text-white px-6 py-1 rounded sm:text-xxs md:text-xs lg:text-xs xl:text-xs xxl:text:lg hover:bg-nav transaction duration-300" type="submit">Save Photo</button>
+            </form>
+        @else
+            <button class="underline cursor-pointer" wire:click="toggleForm">Upload picture here</button>
+        @endif
+ 
 </div>
