0e74fa1c6122d11c010e62a39c9ad800938c2f51
Martijn Jongman <martijnjongman9@gmail.com>
2021-01-05 15:20:50 +0100

    cleanup login pages code

--- resources/views/livewire/components/login/login-component.blade.php
+++ resources/views/livewire/components/login/login-component.blade.php
@@ -1,7 +1,6 @@
 <div class="grid grid-cols-4 grid-row-3 justify-center bg-white w-full h-screen overflow-hidden">
 
-    <div
-        class="relative col-start-1 w-3/4 h-full row-span-3 transition duration-500 ease-in-out transform origin-left hover:scale-150 hover:text-red">
+    <div class="relative col-start-1 w-3/4 h-full row-span-3 transition duration-500 ease-in-out transform origin-left hover:scale-150">
 
         <div class="absolute inset-0 bg-auto bg-center"
             style="background-image: url(https://media-exp1.licdn.com/dms/image/C561BAQHxE9SHfKz_6g/company-background_10000/0?e=2159024400&v=beta&t=XlVltuksEsDRsDWMCskgcScmTKixqgUePDTBuqqbEYE)"></div>
@@ -22,8 +21,9 @@
 
     </div>
 
-    <h1 class="col-start-3 row-start-1 items-end text-nav font-bold text-6xl sm:text-4xl flex justify-center">Log
-        in</h1>
+    <h1 class="col-start-3 row-start-1 items-end text-nav font-bold text-6xl sm:text-xl md:text-3xl lg:text-5xl flex justify-center">
+        Log in
+    </h1>
 
     <form class="col-start-3 row-start-2 flex flex-col justify-center px-20 sm:px-1 md:px-1 text-nav"
           wire:submit.prevent="submit">
@@ -46,14 +46,14 @@
             @error('credentials') <span class="error text-red-500">{{ $message }}</span> @enderror
         </div>
         <div class="flex justify-end">
-            <a class="inline-block font-bold text-sm sm:text-xxs xxl:text-xl  text-gray-600 hover:text-blue-800 underline"
+            <a class="inline-block font-bold text-sm sm:text-xxs xxl:text-xl text-gray-600 hover:text-blue-800 underline"
                href="{{route('login.forgotpassword')}}">
                 Forgot password?
             </a>
         </div>
         <div class="flex justify-center">
             <button
-                class="bg-nav hover:bg-hovBlue text-white text-xs xxl:text-2xl  py-2 px-8 rounded-lg focus:outline-none focus:shadow-outline mt-12 transition duration-200 ease-in-out"
+                class="bg-nav hover:bg-hovBlue text-white text-xs xxl:text-2xl py-2 px-8 rounded-lg focus:outline-none focus:shadow-outline mt-12 transition duration-200 ease-in-out"
                 type="submit">
                 Login
             </button>
@@ -62,7 +62,7 @@
 
     <div class="col-start-3 row-start-3 grid justify-center items-end">
         <a href="{{route('company.register')}}"
-           class="text-gray-600 underline font-bold text-xs sm:text-xxs xxl:text-xl  pb-4 cursor-pointer hover:text-blue-800">
+           class="text-gray-600 underline font-bold text-xs sm:text-xxs xxl:text-xl pb-4 cursor-pointer hover:text-blue-800">
             Don't have an account? Get onboard here!
         </a>
     </div>
