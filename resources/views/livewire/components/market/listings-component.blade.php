2012924d3c6294ee9f0005ce70459e911fc10345
ThowV <thomasvisscher@outlook.com>
2021-01-13 21:32:05 +0100

    Add permission check to listing viewing

--- resources/views/livewire/components/market/listings-component.blade.php
+++ resources/views/livewire/components/market/listings-component.blade.php
@@ -162,18 +162,22 @@
                             </td>
 
                             <td class="flex items-center w-full">
-                                @if(!$trade->responder_id && $trade->owner_id != auth()->id())
-                                <button
-                                    class="w-24 md:w-20 sm:w-14 bg-blue-100 hover:bg-hovBlue border-2 border-hovBlue text-hovBlue hover:text-white text-xs sm:text-xxs xxl:text-2xl py-1 rounded-lg transition duration-300 ease-in-out"
-                                    wire:click="openListing({{ $trade }})">
-                                    {{ $trade->trade_type === "offer" ? "Buy" : "Sell" }}
-                                </button>
+                                @can('listings.read')
+                                    @if(!$trade->responder_id && $trade->owner_id != auth()->id())
+                                    <button
+                                        class="w-24 md:w-20 sm:w-14 bg-blue-100 hover:bg-hovBlue border-2 border-hovBlue text-hovBlue hover:text-white text-xs sm:text-xxs xxl:text-2xl py-1 rounded-lg transition duration-300 ease-in-out"
+                                        wire:click="openListing({{ $trade }})">
+                                        {{ $trade->trade_type === "offer" ? "Buy" : "Sell" }}
+                                    </button>
+                                    @else
+                                    <button
+                                        class="w-24 md:w-20 sm:w-14 bg-gray-100 hover:bg-gray-500 border-2 border-gray-500 text-gray hover:text-white text-xs sm:text-xxs xxl:text-2xl py-1 rounded-lg transition duration-300 ease-in-out"
+                                        wire:click="openListing({{ $trade }})">
+                                        Own
+                                    </button>
+                                    @endif
                                 @else
-                                <button
-                                    class="w-24 md:w-20 sm:w-14 bg-gray-100 hover:bg-gray-500 border-2 border-gray-500 text-gray hover:text-white text-xs sm:text-xxs xxl:text-2xl py-1 rounded-lg transition duration-300 ease-in-out"
-                                    wire:click="openListing({{ $trade }})">
-                                    Own
-                                </button>
+                                    Not permitted
                                 @endif
                             </td>
 
