8ab9efe74cf8d6dae47378be0e682b060ea6e3dd
ThowV <thomasvisscher@outlook.com>
2021-01-19 13:27:25 +0100

    Add vertical lines to charts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,17 +53,30 @@
                 @stack('scripts_onload')
             }
 
-            const overlay = document.querySelector('#settings')
-            const selectBtn = document.querySelector('#settings-btn')
-            const closeBtn = document.querySelector('#close-settings')
-
-            const toggleSetting = () => {
-                overlay.classList.toggle('hidden')
-                overlay.classList.toggle('grid')
-            }
-
-            selectBtn.addEventListener('click', toggleSetting)
-            closeBtn.addEventListener('click', toggleSetting)
+            //Add vertical line to hover effect
+            Chart.defaults.LineWithLine = Chart.defaults.line;
+            Chart.controllers.LineWithLine = Chart.controllers.line.extend({
+                draw: function (ease) {
+                    Chart.controllers.line.prototype.draw.call(this, ease);
+                    if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
+                        var activePoint = this.chart.tooltip._active[0],
+                            ctx = this.chart.ctx,
+                            x = activePoint.tooltipPosition().x,
+                            topY = this.chart.legend.bottom,
+                            bottomY = this.chart.chartArea.bottom;
+                        // draw line
+                        ctx.save();
+                        ctx.beginPath();
+                        ctx.moveTo(x, topY);
+                        ctx.lineTo(x, bottomY);
+                        ctx.lineWidth = 2;
+                        //HOVER VERTICAL LINE COLOR
+                        ctx.strokeStyle = '#07C';
+                        ctx.stroke();
+                        ctx.restore();
+                    }
+                }
+            });
         </script>
 
     </body>
